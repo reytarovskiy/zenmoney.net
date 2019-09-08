778f230018f7333e2682d333cf34ae26cc2527d6
reytarovskiy <reitarovskii.toh@gmail.com>
2019-09-08 14:08:19 +0300

    feat(request): added construct to request object

--- Zenmoney/Request.cs
+++ Zenmoney/Request.cs
@@ -1,11 +1,22 @@
 using System;
+using Newtonsoft.Json;
 
 namespace Zenmoney 
 {
     public class Request
     {
+        [JsonIgnore]
         public string AuthToken { get; set; }
         public Int32 CurrentClientTimestamp { get; set; }
         public Int32 LastServerTimestamp { get; set; }
+
+        public Request() { }
+
+        public Request(string authToken, Int32 currentTimestamp, Int32 lastServerTimestamp)
+        {
+            this.AuthToken = authToken;
+            this.CurrentClientTimestamp = currentTimestamp;
+            this.LastServerTimestamp = lastServerTimestamp;
+        }
     }
 }
