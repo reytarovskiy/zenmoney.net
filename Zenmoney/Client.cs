86441ca077bbdf5f3fc8ef5a56d2b779a26b3e40
reytarovskiy <reitarovskii.toh@gmail.com>
2019-09-08 15:11:05 +0300

    feat(client): send request from client

--- Zenmoney/Client.cs
+++ Zenmoney/Client.cs
@@ -1,10 +1,29 @@
+using System.Net.Http;
+using System.Threading.Tasks;
+
 namespace Zenmoney
 {
-    public class Client 
+    public class Client
     {
-         public SyncResult Sync(Request request)
-         {
-             return new SyncResult();
-         }
+        private string url;
+        private HttpClient httpClient;
+
+        public Client(string url = "http://api.zenmoney.ru/v8/diff/")
+        {
+            this.url = url;
+
+            this.httpClient = new HttpClient();
+        }
+
+        public async Task<SyncResult> Sync(Request request)
+        {
+            var httpRequest = HttpRequestBuilder.CreateFromRequest(this.url, request);
+
+            var response = await this.httpClient.SendAsync(httpRequest);
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            return new SyncResult();
+        }
     }
 }
